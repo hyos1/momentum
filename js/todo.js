d3771480d4e04ce1b74cfc31d60ac5b0ea6541b9
hyos <hyozzang99@gmail.com>
2026-03-26 20:39:38 +0900

    Feat: 식별된 id를 filter()함수로 localStorage에서도 삭제

--- js/todo.js
+++ js/todo.js
@@ -12,6 +12,8 @@ function saveTodos() {
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+  saveTodos();
 }
 
 function paintTodo(newToDoObject) {
